e2e922b5bbfb22558a90a0298bc6e3fdac3db0d2
AkritiUpadhaya <upadhayaakriti23@gmail.com>
2025-04-26 20:27:29 +0545

    homescreen ui implementation

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,53 @@
-import { View, Text } from 'react-native'
+import { View, Text, Button, StyleSheet, TextInput } from 'react-native'
 import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { router } from 'expo-router'
 
 export default function index() {
   return (
+    <SafeAreaView style={{flex:1}}>
     <View>
-      <Text>index</Text>
+        <View style={styles.container}>
+            <Text style={styles.card}>
+                Add new note
+            </Text>
+
+            <TextInput placeholder='Enter note title' style={styles.input}/>
+            <TextInput placeholder='Enter note description' style={styles.input}/>
+        </View>
+      <Button title='Add new note' onPress={()=>{router.push('/note')}}/>
+      <Button title='Cancel' onPress={()=>{}}/>
     </View>
+    </SafeAreaView>
   )
 }
+
+
+const styles= StyleSheet.create({
+    container:{
+        width:'90%',
+        height:200,
+        backgroundColor:'white',
+        shadowColor:'rgba(0,0,0,0.5)',
+        shadowOpacity: 5,
+    alignSelf:'center',
+    padding:10,
+    },
+
+    card:{
+        alignSelf:'center',
+        fontSize:16,
+        fontWeight:'bold',
+        marginBottom:10
+    },
+
+    input:{
+        height:40,
+        borderColor:'gray',
+        borderWidth:1,
+        marginBottom:10,
+        paddingHorizontal:10,
+        marginTop:10
+        
+    }
+})
---
+++ app/note.tsx
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function note() {
+    const addNote=()=>{}
+    const updateNote=()=>{}
+    const deleteNote=()=>{}
+  return (
+    <View>
+      <Text>note is here</Text>
+    </View>
+  )
+}
